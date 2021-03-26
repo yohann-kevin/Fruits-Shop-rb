e518f7f6b2358ca6b2d426ccfea3f33f4b4d6265
Kirua <yo44prg@icloud.com>
2021-03-26 15:51:11 +0100

    feat : modify reduction.rb

--- lib/reduction.rb
+++ lib/reduction.rb
@@ -2,7 +2,12 @@ class Reduction
   $cherryReduction = 0.20
 
   def count_basket
-    [$basket.count("cherry"),$basket.count("banana"),$basket.count("apple"),$basket.count("apfel")]
+    [
+      $basket.count("cherry"),
+      $basket.count("banana"),
+      $basket.count("apple"),
+      $basket.count("apfel")
+    ]
   end
 
   def check_entry(entry, fruits)
