e6f31f044e8001d6be788deaa727c4ee9da12454
kirua <yo44prg@icloud.com>
2021-04-04 17:28:03 +0200

    feat: add test for priceformat

---
+++ spec/priceformat_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+require "priceformat"
+require "reduction"
+
+RSpec.describe Priceformat do
+  it "return 1.0" do
+    price_format = Priceformat.new("euro")
+    expect(price_format.check_format(100)).to eql(1.0)
+  end
+
+  it "return 1.17" do
+    price_format = Priceformat.new("dollars")
+    expect(price_format.check_format(100)).to eql(1.17)
+  end
+
+  it "return 130.01" do
+    price_format = Priceformat.new("yen")
+    expect(price_format.check_format(100)).to eql(130.01)
+  end
+
+  it "return 0.85" do
+    price_format = Priceformat.new("livre")
+    expect(price_format.check_format(100)).to eql(0.85)
+  end
+
+  it "return 7.68" do
+    price_format = Priceformat.new("yuan")
+    expect(price_format.check_format(100)).to eql(7.68)
+  end
+end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 require "price"
 require "reduction"
 
-RSpec.describe Price do
-  it "return 580" do
-    reduction = Reduction.new
-    price = Price.new(reduction)
-    arg = "apfel, apple, apple, pommes, apple, apfel, cherry, cherry"
-    expect(price.get_price(arg)).to eql(580)
-  end
-end
+# RSpec.describe Price do
+#   it "return vous devez payer 580 centimes 🍒" do
+#     reduction = Reduction.new
+#     price = Price.new(reduction)
+#     arg = "apfel, apple, apple, pommes, apple, apfel, cherry, cherry"
+#     expect(price.get_price(arg, "centimes", false)).to eql("vous devez payer 580 centimes 🍒")
+#   end
+# end
